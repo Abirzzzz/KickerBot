7677d47005fe66f53c4e2ce476f4fca3c4671787
Abirzzzz <abirrrrzzzz@gmail.com>
2026-03-29 00:42:20 +0600

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-echo "[SETUP] Installing required packages..."
-echo "----------------------------------------"
-npm install
-
-echo
-read -p "Setup completed. Press enter to continue..."
